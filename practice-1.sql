90d48d7700f72d062c80bd02251f6d983e897801
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-01-17 18:49:04 +0000

    Completed practice-1

--- practice-1.sql
+++ practice-1.sql
@@ -1,7 +1,12 @@
 -- How many reviews are there?
-
+Temporary written code to practice saving 16JAN23.
 -- +----------+
 -- | 302750   |
 -- +----------+
+sqlite3 airbnb.sqlite3
+.read import.sql
+.schema
+SELECT COUNT(*)FROM reviews;
+
 
 
